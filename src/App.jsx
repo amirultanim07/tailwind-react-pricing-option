f3eba25686cf1086dea6a2ad5fcedfe8c190ee4e
amirultanim07 <amirultanim@gmail.com>
2025-04-13 08:51:39 -0600

    added pricing

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
+import { Suspense } from 'react'
 import './App.css'
-import NavBar from './components/Navbar/Navbar'
+import NavBar from './components/NavBar/NavBar'
+import PricingOptions from './components/PricingOptions/PricingOptions'
+import ResultsChart from './components/ResultsChart/ResultsChart'
+import axios from 'axios'
+import MarksChart from './components/MarksChart/MarksChart'
+
+
+const pricingPromise = fetch('pricingData.json').then(res => res.json());
+const marksPromise = axios.get('marksData.json');
 
 
 function App() {
@@ -9,6 +18,18 @@ function App() {
       <header>
         <NavBar></NavBar>
       </header>
+      <main>
+        <Suspense fallback={<span className="loading loading-spinner loading-lg"></span>}>
+          <PricingOptions pricingPromise={pricingPromise}></PricingOptions>
+        </Suspense>
+
+        <Suspense fallback={<span className="loading loading-spinner loading-lg"></span>}>
+          <MarksChart marksPromise={marksPromise}></MarksChart>
+        </Suspense>
+
+        <ResultsChart></ResultsChart>
+
+      </main>
     </>
   )
 }
